20d8182a04adae09eda30154a5418d6eadeb0b06
stivenrodriguezm <stivenrodriguezm@outlook.com>
2021-05-08 19:44:49 -0500

    make it better

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express()
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended:false}))
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
