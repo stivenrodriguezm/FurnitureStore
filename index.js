d229e7f113eb6bd9c9ced4fa5c24d3a751c9d679
stivenrodriguezm <stivenrodriguezm@outlook.com>
2021-05-08 19:26:58 -0500

    make it better

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express()
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended:false}))
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 app.post("/api/form", (req,res) => {
     nodemailer.createTestAccount((err, account) => {
